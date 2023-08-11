0afe8547e8af80e3f1d67a0cd064443cc7af25bf
Yurii Flys <flysyurii@gmail.com>
2023-08-11 14:56:25 +0300

    Player class

---
+++ src/main/java/game/tictactoeproject/Logic/Player.java
@@ -0,0 +1,20 @@
+package game.tictactoeproject.Logic;
+
+public class Player {
+    private String name;
+    private char sign;
+
+    public Player(String name, char sign) {
+        this.name = name;
+        this.sign = sign;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSign() {
+        return sign;
+    }
+
+}
